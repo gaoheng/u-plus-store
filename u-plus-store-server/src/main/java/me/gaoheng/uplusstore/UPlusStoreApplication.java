e3a965a8c6288124ba16120145c3f8e390103283
高恒 <heng.gao@baidao.com>
2018-05-11 09:23:42 +0800

    Initial commit

---
+++ u-plus-store-server/src/main/java/me/gaoheng/uplusstore/UPlusStoreApplication.java
@@ -0,0 +1,12 @@
+package me.gaoheng.uplusstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UPlusStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UPlusStoreApplication.class, args);
+    }
+}
